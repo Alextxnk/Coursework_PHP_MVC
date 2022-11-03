62fc51b4f9ec02822cfffc8f2686b7b275a0201c
Alextxnk <alexsolov766@gmail.com>
2022-11-03 18:14:15 +0300

    change file

--- views/index.view.php
+++ views/index.view.php
@@ -1,115 +1,75 @@
-<!DOCTYPE html>
-<html lang="ru">
+<?php 
+require __DIR__ . "./partials/_header.php";
+?>
 
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
-    <link rel="stylesheet" href="../public/css/style.css">
-    <link rel="shortcut icon" href="../public/img/favicon_store.png">
-    <title>iStockphoto</title>
-    <meta name="description" content="Магазин Фототехники в Ростове-на-Дону">
-</head>
-
-<body>
-<main>
-    <section class="section-about">
-        <ul class="about-ul">
-            <li class="logo-li">
-                <a href="#">
-                    <img class="logo" src="../public/img/logo.png" alt="logo">
-                </a>
-            </li>
-            <hr class="sidebar-divider">
-            <li class="about-li">
-                <a class="sidebar-link" href="#">
-                    <span>Главная</span>
-                </a>
-            </li>
-            <hr class="sidebar-divider">
-            <li class="about-li">
-                <a class="sidebar-link" href="#">
-                    <span>Об авторе</span>
-                </a>
-            </li>
-            <hr class="sidebar-divider">
-            <li class="about-li">
-                <a class="sidebar-link" href="#">
-                    <span>О фирме</span>
-                </a>
-            </li>
+<section class="section-main">
+    <nav>
+        <ul class="nav-ul">
+            <li class="nav-li"><a class="navbar-brand" href="#">Главная</a></li>
         </ul>
-    </section>
 
-    <section class="section-main">
-        <nav>
-            <ul class="nav-ul">
-                <li class="nav-li"><a class="navbar-brand" href="#">Главная</a></li>
-            </ul>
-
-            <ul class="nav-ul">
-                <li class="nav-li"><a class="authorize-link" href="#">Авторизоваться</a></li>
-            </ul>
-        </nav>
-        <div class="content">
-            <div class="row">
-                <div class="card-item">
-                    <div class="card-header">
-                        <div class="row">
-                            <div class="col">
-                                <h1 class="h1">Фотоаппараты</h1>
-                            </div>
+        <ul class="nav-ul">
+            <li class="nav-li"><a class="authorize-link" href="#">Авторизоваться</a></li>
+        </ul>
+    </nav>
+    <div class="content">
+        <div class="row">
+            <div class="card-item">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col">
+                            <h1 class="h1">Фотоаппараты</h1>
                         </div>
-                    </div>
-                    <div class="card-body">
-                        <a href="#">
-                            <img class="photo_equipment" src="../public/img/photo_camera.jpg" alt="Фотоаппараты">
-                        </a>
                     </div>
                 </div>
-                <div class="card-item">
-                    <div class="card-header">
-                        <div class="row">
-                            <div class="col">
-                                <h1 class="h1">Объективы</h1>
-                            </div>
+                <div class="card-body">
+                    <a href="#">
+                        <img class="photo_equipment" src="../public/img/photo_camera.jpg" alt="Фотоаппараты">
+                    </a>
+                </div>
+            </div>
+            <div class="card-item">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col">
+                            <h1 class="h1">Объективы</h1>
                         </div>
-                    </div>
-                    <div class="card-body">
-                        <a href="#">
-                            <img class="photo_equipment" src="../public/img/lens.jpg" alt="Объективы">
-                        </a>
                     </div>
                 </div>
-                <div class="card-item">
-                    <div class="card-header">
-                        <div class="row">
-                            <div class="col">
-                                <h1 class="h1">Вспышки</h1>
-                            </div>
+                <div class="card-body">
+                    <a href="#">
+                        <img class="photo_equipment" src="../public/img/lens.jpg" alt="Объективы">
+                    </a>
+                </div>
+            </div>
+            <div class="card-item">
+                <div class="card-header">
+                    <div class="row">
+                        <div class="col">
+                            <h1 class="h1">Вспышки</h1>
                         </div>
                     </div>
-                    <div class="card-body">
-                        <a href="#">
-                            <img class="photo_equipment" src="../public/img/flash.jpg" alt="Вспышки">
-                        </a>
-                    </div>
+                </div>
+                <div class="card-body">
+                    <a href="#">
+                        <img class="photo_equipment" src="../public/img/flash.jpg" alt="Вспышки">
+                    </a>
                 </div>
             </div>
         </div>
-        <footer>
-            <div class="footer-container">
-                <div>
-                    <span>Copyright &copy; iStockphoto, 2010-2022. All rights reserved</span>
-                </div>
-                <div>
-                    <a class="email" href="mailto:mail@example.com">email</a>
-                </div>
+    </div>
+    <footer>
+        <div class="footer-container">
+            <div>
+                <span>Copyright &copy; iStockphoto, 2010-2022. All rights reserved</span>
             </div>
-        </footer>
-    </section>
-</main>
+            <div>
+                <a class="email" href="mailto:mail@example.com">email</a>
+            </div>
+        </div>
+    </footer>
+</section>
 
-</body>
-</html>
+<?php 
+require __DIR__ . "./partials/_footer.php";
+?>
